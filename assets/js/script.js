0904cefff2345e4b25660ec470377518a4d09e03
devildex91 <neall.t.dexter@gmail.com>
2026-03-03 10:11:17 +0000

    display divide function and subtract question altered for better readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,10 +96,17 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 function displaySubtractQuestion(operand1, operand2) {
+  if (operand2 > operand1){
+    document.getElementById("operand1").textContent = operand2;
+    document.getElementById("operand2").textContent = operand1;
+  } else {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
-  document.getElementById("operator").textContent = "-";
-}
+  
+  }
+    document.getElementById("operator").textContent = "-";
+  }
+
 function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
@@ -107,7 +114,13 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
+ if (operand2 > operand1){
+    document.getElementById("operand1").textContent = operand2;
+    document.getElementById("operand2").textContent = operand1;
+  } else {
   document.getElementById("operand1").textContent = operand1;
   document.getElementById("operand2").textContent = operand2;
-  document.getElementById("operator").textContent = "/";
+  
+  }
+    document.getElementById("operator").textContent = "/";
 }
